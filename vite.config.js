1ee910382f102d498559f47aeefa134b34357427
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-05-16 19:37:29 +0200

    nehe

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: "/LeksakerJSXUppgift/",
+  base: "/JSX-Slutuppgift/",
   plugins: [react()],
 }));
