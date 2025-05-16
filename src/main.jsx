17f6486bb6f6ac7675708cd5dc6eabab6a7cceda
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-05-16 19:10:53 +0200

    ashyagu

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { CartProvider } from "./components/CartContext";
+import { CartProvider } from "./components/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
